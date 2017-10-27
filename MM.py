db1b5a4cab2f5aa8918141dce2820c6058c33519
Unknown <shaylalane@outlook.com>
2017-10-27 15:31:24 -0700

    aj

---
+++ MM.py
@@ -0,0 +1,4 @@
+from random import randint
+options=('Red','Green','Blue','Purple')
+human= input('Enter Red "R", Blue "B", Green "G", or Purple "P" in order up to 4')
+computer=options[randint(0,5)]
