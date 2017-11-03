f238ebb45a9fa0efea1a5f2a0be1957c45658908
Unknown <shaylalane@outlook.com>
2017-11-02 19:46:11 -0700

    mt

---
+++ functiontester.py
@@ -0,0 +1,13 @@
+import random
+computer= [random.choice(['R','G','B','P'])
+
+human = input('Guess up to 4 colors')
+
+if userGuess[0]==computer[0]
+           print("B")
+    elif userGuess [1] == computer [1]
+           print("B")
+    elif userGuess [2] == computer [2]
+            print("B")
+    elif userGuess [3] == computer [3]
+            print("B")
