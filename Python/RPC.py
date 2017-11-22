5f8782ceaed74c125b16e12e6afb1f1e8a301300
Unknown <slane2@cocc.edu>
2017-11-21 17:18:38 -0800

    ripple

--- Python/RPC.py
+++ Python/RPC.py
@@ -1,19 +1,22 @@
 from random import randint
 options=('Rock','Paper', 'Scissors')
-human= input('Rock, Paper, or Scissors?')
 computer=options[randint(0,2)]
 print(computer)
-if human==computer:
-    print('Tie!');
-elif human=='Rock' and computer=='Paper':
-    print("Sorry Sucka!");
-elif human=='Rock' and computer=='Scissors':
-    print('You Win!');
-elif human=='Paper' and computer=='Scissors':
-    print('Sorry Sucka!');
-elif human=='Paper' and computer=='Rock':
-    print('You Win!');
-elif human=='Scissors' and computer=='Rock':
-    print('Sorry Sucka!');
-elif human=='Scissors' and computer=='Paper':
-    print('You Win!');
+count=0
+while count < 6:
+    human= input('Rock, Paper, or Scissors?')
+    if human==computer:
+        print('Tie!');
+    elif human=='Rock' and computer=='Paper':
+        print("Sorry Sucka!");
+    elif human=='Rock' and computer=='Scissors':
+        print('You Win!');
+    elif human=='Paper' and computer=='Scissors':
+        print('Sorry Sucka!');
+    elif human=='Paper' and computer=='Rock':
+        print('You Win!');
+    elif human=='Scissors' and computer=='Rock':
+        print('Sorry Sucka!');
+    elif human=='Scissors' and computer=='Paper':
+        print('You Win!');
+    count=count+1
--- Python/mastermind.py
+++ Python/mastermind.py
@@ -5,40 +5,47 @@ second = random.choice(options)
 third = random.choice(options)
 fourth = random.choice(options)
 computer=[first, second, third, fourth]
+originalcomputer=list(computer)
 print(computer)
-human= input('Make a guess')
-human=list(human)
-count=0
-for i in range(4):
-    if computer == human:
-        print('Huzzah!')
-        break
-for i in range(4):
-    if computer[i]==human[i]:
-        human[i]="T"
-        computer[i]="L"
-        count=count+1
-Y=("B"*count)
-count1=count
-print(computer)
-print(human)
+human="C"
+while human!= computer:
+    human= input('Make a guess')
+    human=list(human)
+    count=0
+    for i in range(4):
+        if computer == human:
+            print('Huzzah!')
+            print(computer)
+            print(human)
+            break
+    for i in range(4):
+        if computer[i]==human[i]:
+            human[i]="T"
+            computer[i]="L"
+            count=count+1
+    Y=("B"*count)
+    count1=count
+    print(computer)
+    print(human)
 
-for i in range (4):
-    if human[0]==computer[i]:
-        human[0]="S"
-        count=count+1
-for i in range (4):
-    if human[1]==computer[i]:
-        human[1]="S"
-        count=count+1
-for i in range (4):
-    if human[2]==computer[i]:
-        human[2]='S'
-        count=count+1
-for i in range (4):
-    if human[3]==computer[i]:
-        human[3]="S"
-        count=count+1
-X=(count-count1)
-hint=list(("W"*X)+Y)
-print(hint)
+    for i in range (4):
+        if human[0]==computer[i]:
+            human[0]="S"
+            count=count+1
+    for i in range (4):
+        if human[1]==computer[i]:
+            human[1]="S"
+            count=count+1
+    for i in range (4):
+        if human[2]==computer[i]:
+            human[2]='S'
+            count=count+1
+    for i in range (4):
+        if human[3]==computer[i]:
+            human[3]="S"
+            count=count+1
+    X=(count-count1)
+    hint=list(("W"*X)+Y)
+    print(hint)
+    computer=list(originalcomputer)
+
