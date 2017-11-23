7f9dff4d5b66a2b34f16e1c14171810f51fc4e7b
Unknown <shaylalane@outlook.com>
2017-11-23 13:56:53 -0800

    inevitablepain

--- Python/mastermind.py
+++ Python/mastermind.py
@@ -15,25 +15,25 @@ for i in range(4):
         break
 for i in range(4):
     if computer[i]==human[i]:
-        Q=human[i]
-        X=computer[i]
+        human[i]=q
+        computer[i]=x
         count=count+1
 print("B"*count)
 print(computer)
 print(human)
 for i in range (4):
     if human[0]==computer[i]:
-        S=human[0]
+        human[0]=s
         print("W")
 for i in range (4):
     if human[1]==computer[i]:
         print("W")
-        T=human[1]
+        human[1]=t
 for i in range (4):
     if human[2]==computer[i]:
         print("W")
-        U=human[2]
+        human[2]=u
 for i in range (4):
     if human[3]==computer[i]:
         print("W")
-        V=human[3]
+        human[3]=v
