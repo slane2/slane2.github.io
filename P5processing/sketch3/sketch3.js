32918a7a3313d027d17f57309badfdbafcf80822
Unknown <shaylalane@outlook.com>
2017-11-30 04:30:47 -0800

    triangle

--- P5processing/sketch3/sketch3.js
+++ P5processing/sketch3/sketch3.js
@@ -1,6 +1,14 @@
 function setup() {
-    createCanvas(770,570);
+    createCanvas(499,499);
 }
 function draw(){
-    if ()
+    background(200,160,230);
+    point(249,249);
+    point(0,0);
+    point(499,499);
+    point(0,499);
+    point(499,0);
+    triangle(0,0,499,0,249,499);
+    triangle(0,499, 0, 19, 230, 499);
+    
 }
